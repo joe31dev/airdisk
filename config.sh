ddf553889c243829f1d54c53bba78f7fd45109f2
joe31dev <94455703+joe31dev@users.noreply.github.com>
2024-01-04 22:15:56 +0100

    s

--- config.sh
+++ config.sh
@@ -10,58 +10,30 @@ apt-get -y upgrade
 apt-get -y install samba samba-common-bin hostapd dnsmasq
 
 
-
-# add lines to dhcpd.conf file
-echo >>/etc/dhcpd.conf <<EOF 
-interface wlan0
-static ip_address=192.168.0.254/24
-nohook wpa_supplicant
-EOF
-
-# configure address space
-echo > /etc/dnsmasq.conf <<EOF 
-# define interface
-interface=wlan0
-# dhcp address space
-dhcp-range=192.168.0.1,192.168.0.100,255.255.255.0,24h 
-EOF 
+#configure dhcp
+wget https://github.com/joe31dev/airdisk/raw/main/dhcp.conf
+sudo cat dhcp.conf >> /etc/dhcp.conf
 
 # configure hostapd
-echo > /etc/hostapd/hostapd.conf <<EOF 
-# interface wlan 
-interface=wlan0
+wget https://github.com/joe31dev/airdisk/raw/main/hostapd.conf
+sudo cp hostapd.conf /etc/hostapd.conf
+sudo echo 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' > /etc/default/hostapd
 
-# nl80211 driver for pi
-driver=nl80211
+# configure dnsmasq
+wget https://github.com/joe31dev/airdisk/raw/main/dnsmasq.conf
+sudo cp dnsmasq.conf /etc/dnsmasq.conf
 
-# SSID
-ssid=AirDisk
 
-# mode  : a = IEEE 802.11a (5GHz) , b = IEEE 802.11b (2.4GHz), g = IEEE 802.11g) (2.4GHz)
-hw_mode=g
-
-# channel (1-14)
-channel=6
-EOF 
-
-echo 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' > /etc/default/hostapd
-
-# configure hostapd
-systemctl unmask hostapd
-systemctl enable hostapd
+# configure hostapd service
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
 
 # create shared space
 mkdir /var/samba
 
 # configure samba
-cat <<EOF 
-[airdisk]
-path = /var/samba
-writeable=Yes
-create mask=0777
-directory mask=0777
-public=no
-EOF >> /etc/samba/smb.conf
+wget https://github.com/joe31dev/airdisk/raw/main/smb.conf
+sudo cat smb.conf >> /etc/samba/smb.conf
 
 # reboot pi to launch services
 reboot
